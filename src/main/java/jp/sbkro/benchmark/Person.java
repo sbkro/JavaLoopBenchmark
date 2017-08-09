dc09afd1c68d9c8aabe4671d9979ce9823e81d9d
sbkro <sbkro.apps@gmail.com>
2017-08-10 07:18:42 +0900

    add gradle script. add arraylist benchmark code.

---
+++ src/main/java/jp/sbkro/benchmark/Person.java
@@ -0,0 +1,25 @@
+package jp.sbkro.benchmark;
+
+public class Person {
+
+    private int no;
+
+    private String name;
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
